1c3b35eb089b51754ea2e8ca98aecc8ed2b467ed
tanutasir <tanutasir@inbox.lv>
2017-07-26 20:30:09 +0300

    Initial commit

---
+++ project/resources/views/layouts/inc/scripts.blade.php
@@ -0,0 +1,11 @@
+<script src="{{ asset('js/app.js') }}"></script>
+{{--<script src="{{ asset('') }}"></script>--}}
+{{--<script src="{{ asset('') }}"></script>--}}
+{{--<script src="{{ asset('') }}"></script>--}}
+<script src="{{ asset('js/script.js') }}"></script>
+
+
+
+
+
+
---
+++ project/resources/views/layouts/inc/styles.blade.php
@@ -0,0 +1,5 @@
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+{{--<link href="{{ asset('') }}" rel="stylesheet">--}}
+{{--<link href="{{ asset('') }}" rel="stylesheet">--}}
+{{--<link href="{{ asset('') }}" rel="stylesheet">--}}
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
